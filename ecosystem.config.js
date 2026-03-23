f56e951c92b378634071023a470d6e1f03954e85
ken <kimkhay8@gmail.com>
2026-03-23 10:28:03 +0800

    Deployment: Status - not working

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-    apps: [
-      {
-        name: "cataloger-app",
-        script: "server.js",
-        cwd: ".next/standalone",
-        exec_mode: "fork",
-        instances: 1,
-        env: {
-          NODE_ENV: "production",
-          PORT: 3000,
-          JWT_SECRET:"FOqDtFjZ&-=#0)Zfs#(@,BuW=nlk&MS[!xXmcAEy3-c",
-          NODE_ENV_BACKEND_API: "http://10.2.42.18:8001",
-          NEXT_PUBLIC_BACKEND_API:" http://fastapi_app:8001"
-        }
-      }
-    ]
-  };
+  apps: [
+    {
+      name: "cataloger-app",
+      script: "pnpm",
+      args: "start",
+      cwd: "D:\\code\\next\\Library-Manager-2026\\webservices\\cataloger-app",
+      exec_mode: "fork",
+      instances: 1,
+      watch: false,
+      autorestart: true,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        JWT_SECRET: "FOqDtFjZ&-=#0)Zfs#(@,BuW=nlk&MS[!xXmcAEy3-c",
+        NODE_ENV_BACKEND_API: "http://10.2.42.18:8001",
+        NEXT_PUBLIC_BACKEND_API: "http://10.2.42.18:8001",
+      },
+    },
+  ],
+};
